f94301534de842e86dfcb9d81cfcdc3f7ce9b36d
Kosuke Murakami <kosuke.murakami@tier4.jp>
2021-04-02 16:20:22 +0900

    update
    Signed-off-by: Kosuke Murakami <kosuke.murakami@tier4.jp>

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2011, Willow Garage, Inc.
- * Copyright (c) 2017, Open Source Robotics Foundation, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "std_msgs/msg/string.hpp"
 
 #include "rclcpp/rclcpp.hpp"
@@ -37,30 +7,36 @@ class Chatter : public rclcpp::Node
 {
 public:
   Chatter();
-  diagnostic_updater::Updater updater_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  std::mutex lock_;
+
+  void update();
 };
 
 Chatter::Chatter()
-    : Node("chatter"),
-      updater_(this)
+    : Node("chatter")
 {
-  // rclcpp::Rate loop_rate(1);
-  // size_t count_ = 1;
-  // std::unique_ptr<std_msgs::msg::String> msg_;
-  // rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
-  // rclcpp::QoS qos(rclcpp::KeepLast(7));
-  // pub_ = this->create_publisher<std_msgs::msg::String>("chatter", qos);
+  timer_ = rclcpp::create_timer(
+      this->get_node_base_interface(),
+      this->get_node_timers_interface(),
+      this->get_clock(),
+      rclcpp::Duration::from_seconds(1.0),
+      std::bind(&Chatter::update, this));
 
-  // while (rclcpp::ok())
-  // {
-  //   msg_ = std::make_unique<std_msgs::msg::String>();
-  //   msg_->data = "Hello World: " + std::to_string(count_++);
-  //   RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", msg_->data.c_str());
-  //   pub_->publish(std::move(msg_));
+}
 
-  //   rclcpp::spin_some(this->get_node_base_interface());
-  //   loop_rate.sleep();
-  // }
+void Chatter::update()
+{
+  if (rclcpp::ok())
+  {
+    std::unique_lock<std::mutex> lock(lock_);
+    // some process
+    for (int i = 0; i < 10; ++i) {
+      int j = 0;
+      j ++;
+    }
+    RCLCPP_INFO(this->get_logger(),"update is called");
+  }
 }
 
 int main(int argc, char **argv)
@@ -71,7 +47,4 @@ int main(int argc, char **argv)
   executor->add_node(chatter_node);
   executor->spin();
   rclcpp::shutdown();
-
-  // rclcpp::spin(std::make_shared<Chatter>());
-  // rclcpp::shutdown();
 }
